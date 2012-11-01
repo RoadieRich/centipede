61d54ce80addfb2fb64bc003d9800c11f19b569e
Rich Lovely <rich@roadierich.co.uk>
2012-11-01 17:12:29 +0000

    improved functionality of datagridview

--- Centipede/VarDataSet.cs
+++ Centipede/VarDataSet.cs
@@ -10,25 +10,46 @@ namespace Centipede {
         partial class VariablesDataTable
         {
             
-            private Dictionary<String, Object> VarDict = Program.Variables;
+            private Dictionary<String, Variable> VarDict = Program.Variables;
 
             internal void Update()
             {
-                foreach (Variable v in VarDict)
-                {
-                    if (!(this.Where((r) => (r.Name == v.Name)).Any()))
-                    {
-                        AddVariablesRow(v.Name, v.Value);
-                    }
-                    else
-                    {
-                        this.FindByName(v.Name).Value = v.Value;
-                    }
-                }
+                //this.Clear();
+                //foreach (KeyValuePair<String, Variable> kvp in VarDict)
+                //{
+                //    Variable v = kvp.Value;
+                //    if (v.Name == null)
+                //    {
+                //        v.Name = kvp.Key;
+                //    }
+                //    if (v.Index != null)
+                //    {
+                //        if (v.Index >= 0)
+                //        {
+                //            VariablesRow newRow = (VariablesRow)NewRow();
+                //            newRow.Value = v.Value;
+                //            v.Index = newRow.index;
+                //            newRow.Type = (Byte)JobVariable.Types.Other;
+
+                //            AddVariablesRow(newRow);
+                //        }
+                //    }
+                //    else
+                //    {
+                //        VariablesRow row = this.FindByindex(v.Index);
+                //        row.Value =
+                //    }
+                //}
+                Variable console = VarDict["console"];
+                VarDict.Clear();
                 foreach (VariablesRow r in this)
                 {
-                    Program.Variables[r.Name] = r.Value;
+                    VarDict[r.Name] = new Variable();
+                    VarDict[r.Name].Value = r.Value;
+                   // VarDict[r.Name].Index = r.index;
+                    VarDict[r.Name].Name = r.Name;
                 }
+                VarDict["console"] = console;
             }
         }
     }
