776b700dbe3c265c890b92a2be8a38797abc53c0
rlovely <rlovely@localhost>
2012-11-05 17:41:40 +0000

    Cleanup of Compiler Warnings.  3 left that cannot be resolved without recompiling IronPython.

--- Centipede/Program.cs
+++ Centipede/Program.cs
@@ -5,8 +5,6 @@ using System.Windows.Forms;
 
 namespace Centipede
 {
-    
-
     public static class Program
     {
         /// <summary>
@@ -15,7 +13,6 @@ namespace Centipede
         [STAThread]
         static void Main()
         {
-
             //XXX:Testing only
             JobFileName = "Test File";
             SetupTestAction();
@@ -30,45 +27,57 @@ namespace Centipede
             throw new NotImplementedException();
         }
 
-        public class Variable
-        {
-            public Int32 Index;
-            public String Name;
-            public Object Value;
+        //public class Variable
+        //{
+        //    public Int32 Index;
+        //    public String Name;
+        //    public Object Value;
         
-            public Variable(Int32 index, String name, Object value)
-            {
-                Index = index;
-                Name = name;
-                Value = value;
-            }
+        //    public Variable(Int32 index, String name, Object value)
+        //    {
+        //        Index = index;
+        //        Name = name;
+        //        Value = value;
+        //    }
 
-            public Variable()
-            {
-                Name = "(Name)";
-                Value = "(Value)";
-            }
+        //    public Variable()
+        //    {
+        //        Name = "(Name)";
+        //        Value = "(Value)";
+        //    }
 
-            public static explicit operator Variable(KeyValuePair<String, Object> kvp)
-            {
-                return new Variable(-1, kvp.Key, kvp.Value);
-            }
-            public static implicit operator KeyValuePair<String, Object>(Variable v)
-            {
-                return new KeyValuePair<string, object>(v.Name, v.Value);
-            }
-        }
+        //    public static explicit operator Variable(KeyValuePair<String, Object> kvp)
+        //    {
+        //        return new Variable(-1, kvp.Key, kvp.Value);
+        //    }
+        //    public static implicit operator KeyValuePair<String, Object>(Variable v)
+        //    {
+        //        return new KeyValuePair<string, object>(v.Name, v.Value);
+        //    }
+        //}
 
+
+        /// <summary>
+        /// Dictionary of Variables for use by actions.  As much as I'd like to make types more intuitive, 
+        /// I can't figure a way of doing it easily. 
+        /// </summary>
         public static Dictionary<String, Object> Variables = new Dictionary<String, Object>();
-        public static List<Action> Actions = new List<Action>();
+
+        
         public static string JobFileName = "testing.100p";
         public static string JobName = "Testing";
-        private static JobFile File = null;
 
+        private static List<Action> Actions = new List<Action>();
+        //private static JobFile File = null;
 
+        /// <summary>
+        /// Run the job, starting with the first action added.
+        /// </summary>
+        /// <param name="updateCallback"><see cref="UpdateCallback"/></param>
+        /// <param name="completedHandler"><see cref="CompletedHandler"/></param>
+        /// <param name="errorHandler"><see cref="ErrorHandler"/></param>
         internal static void RunJob(UpdateCallback updateCallback = null, CompletedHandler completedHandler = null, ErrorHandler errorHandler = null)
         {
-
             Action currentAction;
 
             if (Actions.Count < 1)
@@ -89,9 +98,8 @@ namespace Centipede
             while (currentAction != null)
             {
                 try
-                {
+                {   currentAction.DoAction();
                     updateCallback(currentAction);
-                    currentAction.DoAction();
                     currentAction = currentAction.GetNext();
                 }
                 catch (ActionException e)
@@ -99,27 +107,45 @@ namespace Centipede
                     if (!errorHandler(e, out currentAction))
                     {
                         completed = false;
+                        break;
                     }
                 }
             }
             completedHandler(completed);
         }
 
+
+        /// <summary>
+        /// Called after executing an action
+        /// </summary>
+        /// <param name="action">The action that has just been executed.</param>
         internal delegate void UpdateCallback(Action action);
+
+        /// <summary>
+        /// Handler for job completion
+        /// </summary>
+        /// <param name="succeeded">True if all actions completed successfully.</param>
         internal delegate void CompletedHandler(Boolean succeeded);
+
+        /// <summary>
+        /// Handler delegate for errors occuring in actions
+        /// </summary>
+        /// <param name="e">The exception that caused the error</param>
+        /// <param name="nextAction">Set to the next action - useful for repeating actions</param>
+        /// <returns>True if execution of Job should continue, false to halt</returns>
         internal delegate Boolean ErrorHandler(ActionException e, out Action nextAction);
 
         internal static void LoadJob()
         {
             throw new NotImplementedException();
 
-            JobName = File.getName();
+            //JobName = File.getName();
         }
 
         static void SetupTestAction()
         {
-            Actions.Add(new PythonAction("Test Action", @"sys.stdout.write(""Hello World!"")"));
-
+            Program.AddAction(new PythonAction("pyact", "i = int(variables['a']); variables['a'] = i+1"));
+            //Program.AddAction(new PythonAction("Test Action", @"sys.stdout.write(""Hello World!"")"));
         }
 
         public static void AddAction(Action action)
@@ -162,14 +188,4 @@ namespace Centipede
 
         public readonly Action ErrorAction = null;
     }
-
-
-    
-
-    //class VarsList
-    //{
-    //    DataMember d;
-    //}
-
-
 }
