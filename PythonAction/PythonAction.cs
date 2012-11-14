2d1676bb02af11cae970a6ba0a0965ea6a2f3641
rlovely <rlovely@localhost>
2012-11-14 14:43:55 +0000

    Splitting Actions into separate assemblies

---
+++ PythonAction/PythonAction.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Centipede;
+using Centipede.PyEngine;
+
+
+namespace Centipede.PyAction
+{
+    [Centipede.ActionCategory("Other")]
+    public class PythonAction : Centipede.Action
+    {
+
+        public PythonAction(Dictionary<String, Object> variables, String source = "")
+            : base("Python Action", variables)
+        {
+            Source = source;
+        }
+
+        [ActionArgument(usage = "Source code to be executed")]
+        public String Source;
+        //{
+        //    get
+        //    {
+        //        return (String)Attributes["source"];
+        //    }
+        //    set
+        //    {
+        //        Attributes["source"] = value;
+        //    }
+        //}
+        public override void DoAction()
+        {
+            PythonEngine engine = PythonEngine.Instance;
+            if (!engine.VariableExists("variables"))
+            {
+                engine.SetVariable("variables", Variables);
+            }
+            try
+            {
+                engine.Execute(Source);
+            }
+            catch (PythonException e)
+            {
+                throw new ActionException(e, this);
+            }
+        }
+    }
+}
