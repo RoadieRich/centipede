d24eab3c527fddf22d0be444018d0db76a6bc0ff
Rich Lovely <rich@roadierich.co.uk>
2012-11-14 14:43:55 +0000

    Splitting Actions into separate assemblies

---
+++ PythonEngine/PythonEngine.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System;
+using IronPython.Hosting;
+using Microsoft.Scripting;
+using Microsoft.Scripting.Hosting;
+using System.Collections.Generic;
+
+namespace Centipede.PyEngine
+{
+    /// <summary>
+    /// The Iron Python Engine.  Use the <code>variables</code> dictionary to access Job variables.
+    /// </summary>
+
+    public class PythonEngine
+    {
+        private ScriptEngine PyEngine = null;
+        private ScriptScope PyScope = null;
+
+        private PythonEngine()
+        {
+            if (PyEngine == null)
+            {
+                PyEngine = Python.CreateEngine();
+                PyScope = PyEngine.CreateScope();
+                PyScope.ImportModule("sys");
+
+            }
+        }
+
+        /// <summary>
+        /// Execute code internally
+        /// </summary>
+        /// <param name="code">The code to execute</param>
+        public void Execute(String code)
+        {
+            ScriptSource source = PyEngine.CreateScriptSourceFromString(code, SourceCodeKind.Statements);
+            CompiledCode compiled = source.Compile();
+            try
+            {
+                compiled.Execute(PyScope);
+            }
+            catch (Exception e)
+            {
+                throw new PythonException(e);
+            }
+        }
+
+        /// <summary>
+        /// Evaluate the expression
+        /// </summary>
+        /// <typeparam name="T">(C#) Type to coerce the value of the expression to</typeparam>
+        /// <param name="expression">Expression to evaluate</param>
+        /// <returns>The result of the expression, coerced to type T</returns>
+        public T Evaluate<T>(String expression)
+        {
+            ScriptSource source = PyEngine.CreateScriptSourceFromString(expression, SourceCodeKind.Expression);
+            CompiledCode compiled = source.Compile();
+            return compiled.Execute<T>(PyScope);
+        }
+
+        /// <summary>
+        /// Set a python variable, inside the Engine.
+        /// </summary>
+        /// <param name="name">Name of the variable to set</param>
+        /// <param name="value">Value to set it to</param>
+        public void SetVariable(String name, Object value)
+        {
+            PyScope.SetVariable(name, value);
+        }
+
+        /// <summary>
+        /// Get the value of a variable inside the python engine.
+        /// </summary>
+        /// <param name="name">Variable name to get</param>
+        /// <returns>The variable's value.  Will need casting to the correct type.</returns>
+        public Object GetVariable(String name)
+        {
+            return PyScope.GetVariable(name);
+        }
+
+        /// <summary>
+        /// Get a python variable, with a known type
+        /// </summary>
+        /// <typeparam name="T">The (c#) type to get the variable as</typeparam>
+        /// <param name="name">Name of the variable to fetch</param>
+        /// <returns>The value of the variable, cast to the appropriate C# type</returns>
+        public T GetVariable<T>(String name)
+        {
+            return PyScope.GetVariable<T>(name);
+        }
+
+        public Boolean VariableExists(String name)
+        {
+            return PyScope.ContainsVariable(name);
+        }
+
+        #region Singleton handling code
+
+        private volatile static PythonEngine _instance;
+        private static Object _syncRoot = new Object();
+
+        /// <summary>
+        /// The PyEngine Singleton.  <seealso href="http://msdn.microsoft.com/en-us/library/ff650316.aspx"/>
+        /// </summary>
+        public static PythonEngine Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_syncRoot)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new PythonEngine();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        #endregion
+    }
+
+    public class PythonException : Exception
+    {
+        public PythonException(Exception e)
+            : base("Python Error: " + e.Message, e)
+        { }
+    }
+}
+
+
---
+++ TextFile/Action.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Centipede;
+using System.IO;
+
+namespace Centipede.TextFile
+{
+    [ActionCategory("Text")]
+    public class OpenTextFile : Action
+    {
+        public OpenTextFile(Dictionary<String, Object> variables) 
+            : base("Open Text File", variables)
+        { }
+
+        [ActionArgument(usage="File to open")]
+        public String Filename = "";
+
+        [ActionArgument(usage = "Allow writing to file")]
+        public Boolean Write = true;
+
+        [ActionArgument(usage = "Allow reading from file")]
+        public Boolean Read = true;
+
+        [ActionArgument(displayName = "File Variable", usage = "Variable to store the opened file, without % signs")]
+        public String FileVarName = "TextFile";
+
+        public override void DoAction()
+        {
+            FileAccess access = Read ? FileAccess.Read : 0;
+            access |= Write ? FileAccess.Write : 0;
+            Variables[ParseStringForVariables(FileVarName)] = File.Open(ParseStringForVariables(Filename),FileMode.OpenOrCreate, access);
+        }
+    }
+
+    [ActionCategory("Text")]
+    public class SaveTextFile : Action
+    {
+        SaveTextFile(Dictionary<String, Object> variables)
+            : base("Save Text File", variables)
+        { }
+
+        [ActionArgument(displayName = "File Variable", usage = "Variable the file is stored in")]
+        public String FileVarName = "TextFile";
+
+        [ActionArgument(displayName = "Close", usage = "Close file after saving")]
+        Boolean CloseAfter = false;
+
+        public override void DoAction()
+        {
+            FileStream fs = Variables[ParseStringForVariables(FileVarName)] as FileStream;
+            
+            fs.Flush();
+
+            if (CloseAfter)
+            {
+                fs.Close();
+            }
+        }
+    }
+
+    [ActionCategory("Text", helpText="Read an entire line from a file")]
+    public class ReadLineFromTextFile : Action
+    {
+        public ReadLineFromTextFile(Dictionary<String, Object> variables)
+            : base("Read Line", variables)
+        { }
+
+        [ActionArgument(displayName = "File Variable", usage = "Variable the file is stored in")]
+        public String FileVarName = "TextFile";
+
+        [ActionArgument(usage = "Read next line in order, or skip to the line below")]
+        public Boolean Sequential = true;
+
+        [ActionArgument(displayName = "Line number")]
+        public Int32 LineNumber = 0;
+
+        [ActionArgument(displayName="Destination Variable", usage="Variable to store the read text in")]
+        public String DestinationVariable = "Line";
+
+        public override void DoAction()
+        {
+            FileStream fs = Variables[ParseStringForVariables(FileVarName)] as FileStream;
+            EasyFileStream sr = new EasyFileStream(fs);
+
+            if (!Sequential)
+            {
+                fs.Seek(LineNumber, SeekOrigin.Begin);
+            }
+
+
+
+            Variables[ParseStringForVariables(DestinationVariable)] = sr.ReadLine();
+
+        }
+    } 
+
+    class EasyFileStream
+    {
+        public EasyFileStream(FileStream fs)
+        {
+            _fs = fs;
+
+            if (fs.CanRead)
+            {
+                _reader = new StreamReader(fs);
+            }
+            if (fs.CanWrite)
+            {
+                _writer = new StreamWriter(fs);
+            }
+        }
+
+        private StreamReader _reader;
+        private StreamWriter _writer;
+        private FileStream _fs;
+
+        public void Write<T>(T data)
+        {
+            _writer.Write(data);
+        }
+
+        public String ReadLine()
+        {
+            return _reader.ReadLine();
+        }
+
+        public void Seek(long offset, SeekOrigin origin)
+        {
+            _fs.Seek(offset, origin);
+
+            
+        }
+    }
+}
